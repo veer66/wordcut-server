2e384b283b26e1706f391afc54224c0e937102bf
Vee Satayamas <vsatayamas@gmail.com>
2018-01-06 22:41:17 +0700

    add more complex dag test

--- test.sh
+++ test.sh
@@ -8,5 +8,9 @@ echo "DAG"
 curl -d '{"text":"กากกา"}' http://localhost:3000/dag
 echo
 echo
+echo "DAG COMPLEX"
+curl -d '{"text":"รอบอก"}' http://localhost:3000/dag
+echo
+echo
 echo "Invalid"
 curl -v -d '{"textx":"กากกา"}' http://localhost:3000/dag
